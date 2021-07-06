efdd78b5f47e5e57a08425db7e154bfbf53d24e3
nezumihole <nezumihole@gmail.com>
2021-07-07 00:11:39 +0900

    initial commit change from node to go

---
+++ example-app/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprintf(writer, "Hello! GAE")
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
